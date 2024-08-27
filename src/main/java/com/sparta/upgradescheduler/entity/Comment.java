b535e8107598073447280d80f44678a9b3405d94
Hyemin Yang <asitwas729@gmail.com>
2024-08-27 19:44:18 +0900

    연관관계, 세부적인 어노테이션 추가

--- src/main/java/com/sparta/upgradescheduler/entity/Comment.java
+++ src/main/java/com/sparta/upgradescheduler/entity/Comment.java
@@ -2,26 +2,27 @@ package com.sparta.upgradescheduler.entity;
 
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "Comment")
 public class Comment extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "comment_id")
     private Long commentId;
+
+    @Column(nullable = false, length = 200)
     private String comment;
+
+    @Column(nullable = false, length = 20)
     private String username;
-    @CreatedDate
-    private LocalDateTime createAt;
-    @LastModifiedDate
-    private LocalDateTime updateAt;
 
     @ManyToOne
-    @JoinColumn(name = "scheduleId")
+    @JoinColumn(name = "schedule_id")
     private Schedule schedule;
+
+    //삭제 여부
+    @Column(nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
+    private Boolean isDeleted = false;
 
 }
--- src/main/java/com/sparta/upgradescheduler/entity/Schedule.java
+++ src/main/java/com/sparta/upgradescheduler/entity/Schedule.java
@@ -3,10 +3,6 @@ package com.sparta.upgradescheduler.entity;
 import com.sparta.upgradescheduler.dto.ScheduleRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,13 +13,24 @@ public class Schedule extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "schedule_id")
     private Long scheduleId;
+
+    @Column(nullable = false, length = 20)
     private String username;
+
+    @Column(nullable = false, length = 20)
     private String title;
+
+    @Column(length = 200)
     private String content;
 
     @OneToMany(mappedBy = "schedule")
-    private List<Comment> commentList = new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>();
+
+    // 삭제 여부
+    @Column(nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
+    private Boolean isDeleted = false;
 
     public Schedule(ScheduleRequestDto dto){
         this.username = dto.getUsername();
