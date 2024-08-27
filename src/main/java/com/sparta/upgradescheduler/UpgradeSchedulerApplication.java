fabc4da120f15a72021f8a1796593cb5ee120afd
Hyemin Yang <asitwas729@gmail.com>
2024-08-27 09:56:13 +0900

    init
    mysql(upgrade_scheduler) 연결

---
+++ src/main/java/com/sparta/upgradescheduler/UpgradeSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.upgradescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradeSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpgradeSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/upgradescheduler/UpgradeSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.upgradescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpgradeSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
